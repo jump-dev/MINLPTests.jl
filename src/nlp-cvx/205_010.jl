4088e1c2965cbc46d561090d658d197108a9e47a
odow <o.dowson@gmail.com>
2019-02-01 13:50:48 -0600

    Fix exp in Julia 1.0

--- src/nlp-cvx/205_010.jl
+++ src/nlp-cvx/205_010.jl
@@ -5,23 +5,22 @@ function nlp_cvx_205_010(optimizer, objective_tol, primal_tol, dual_tol)
     # - exponential cones
     # Variants
     #   010 - intersection set
-    
+
     m = Model(solver=solver)
-    
+
     @variable(m, x)
     @variable(m, y >= 0, start=0.1)
     @variable(m, z)
-    
+
     @objective(m, Max, y)
-    @NLconstraint(m, y*e^(x/y) <= z)
-    @NLconstraint(m, y*e^(-x/y) <= z)
-    @NLconstraint(m, x^2 + y^2 <= -z+5)
-    
+    @NLconstraint(m, y * exp(x / y) <= z)
+    @NLconstraint(m, y * exp(-x / y) <= z)
+    @NLconstraint(m, x^2 + y^2 <= -z + 5)
+
     status = solve(m)
-    
+
     check_status(status)
     check_objective(m, 1.7912878443121907, tol = objective_tol)
     check_solution([x,y,z], [0.0, 1.7912878443121907, 1.7912878443121907], tol = primal_tol)
-    
-end
 
+end

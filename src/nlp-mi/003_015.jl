5ba25359f53f485f6109c1db436763200e7d9dce
François Pacaud <frapac@users.noreply.github.com>
2019-02-06 20:06:34 +0100

    Update solutions of some tests (#10)
    * update precision of numerical results
    
    * update tests to make Ipopt passes

--- src/nlp-mi/003_015.jl
+++ src/nlp-mi/003_015.jl
@@ -1,23 +1,23 @@
 function nlp_mi_003_015(optimizer, objective_tol, primal_tol, dual_tol;
-        termination_target = JuMP.MOI.LOCALLY_SOLVED, 
+        termination_target = JuMP.MOI.LOCALLY_SOLVED,
         primal_target = JuMP.MOI.FEASIBLE_POINT)
     # Test Goals:
     # - quadratic objective as NLobjective and non-linear constraints
-    
+
     m = Model(optimizer)
-    
+
     @variable(m, x, Int)
     @variable(m, y, Int)
-    
+
     @NLobjective(m, Max, x^2 + y)
     @NLconstraint(m, y >= exp(x-2) - 2)
     @NLconstraint(m, y <= sin(x)^2 + 2)
-    
+
     optimize!(m)
-    
+
     check_status(m, termination_target, primal_target)
-    check_objective(m, 11.000000198181866, tol = objective_tol)
+    check_objective(m, 11.0, tol = objective_tol)
     check_solution([x,y], [3, 2], tol = primal_tol)
-    
+
 end
 
--- src/nlp/005_010.jl
+++ src/nlp/005_010.jl
@@ -1,24 +1,24 @@
 function nlp_005_010(optimizer, objective_tol, primal_tol, dual_tol;
-        termination_target = JuMP.MOI.LOCALLY_SOLVED, 
+        termination_target = JuMP.MOI.LOCALLY_SOLVED,
         primal_target = JuMP.MOI.FEASIBLE_POINT)
     # Test Goals:
     # - function /
-    
+
     m = Model(optimizer)
-    
+
     @variable(m, x >= 0)
     @variable(m, y >= 0)
-    
+
     @objective(m, Min, x + y)
     @NLconstraint(m, y >= 1/(x+0.1) - 0.5)
     @NLconstraint(m, x >= y^(-2) - 0.5)
     @NLconstraint(m, 4 / (x+y+0.1) >= 1)
-    
+
     optimize!(m)
-    
+
     check_status(m, termination_target, primal_target)
-    check_objective(m, 1.5449760741521967, tol = objective_tol)
-    check_solution([x,y], [0.5848970571378771, 0.9600790170143196], tol = primal_tol)
-    
+    check_objective(m, 1.5449723904, tol = objective_tol)
+    check_solution([x,y], [0.5848953813, 0.9600770092], tol = primal_tol)
+
 end
 

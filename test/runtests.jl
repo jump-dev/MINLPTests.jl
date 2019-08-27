364c105c0153ea766de2af93b76f28b044c8e8b1
Carleton Coffrin <carleton@coffrin.com>
2019-08-27 10:03:49 -0600

    Minimal Updates for JuMP v0.20 (#18)
    * updates for JuMP v0.20

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,27 +12,17 @@ const MIPOLY_SOLVERS = [JUNIPER]
 
 @testset "JuMP Model Tests" begin
     @testset "$(solver.constructor): nlp" for solver in NLP_SOLVERS
+        #MINLPTests.test_nlp(solver)
         MINLPTests.test_nlp(solver, exclude = [
-            "005_010",  # Ipopt needs relaxed tolerances.
             "005_011",  # Uses the function `\`
-            "008_011"   # Requires quadratic duals
         ])
-        @testset "nlp_cvx_005_010" begin
-            MINLPTests.nlp_005_010(solver, 1e-5, 1e-5, 1e-5)
-        end
-        MINLPTests.test_nlp_cvx(solver, exclude = [
-            "109_011"  # Ipopt needs relaxed tolerances.
-        ])
-        @testset "nlp_cvx_109_011" begin
-            MINLPTests.nlp_cvx_109_011(solver, 1e-7, 1e-6, 1e-7,
-                termination_target = JuMP.MOI.ALMOST_LOCALLY_SOLVED,
-                primal_target = JuMP.MOI.NEARLY_FEASIBLE_POINT)
-        end
+        MINLPTests.test_nlp_cvx(solver)
     end
     @testset "$(solver.constructor): nlp_mi" for solver in MINLP_SOLVERS
+        #MINLPTests.test_nlp_mi(solver)
         MINLPTests.test_nlp_mi(solver, exclude = [
-            "005_011",  # Uses the function `\`
             "003_013",  # Bug in Juniper - handling of expression graph?
+            "005_011",  # Uses the function `\`
             "006_010"   # Bug in Juniper - handling of user-defined functions.
         ])
         MINLPTests.test_nlp_mi_cvx(solver)

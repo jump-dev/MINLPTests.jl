030b5616445384cb07453bda1d97360ed13ff5ee
Oscar Dowson <odow@users.noreply.github.com>
2021-04-12 15:12:01 +1200

    Clean up runtests.jl (#30)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,17 @@
-using MINLPTests, JuMP, Test
+using Ipopt
+using JuMP
+using Juniper
+using MINLPTests
+using Test
 
-using Ipopt, Juniper
-const IPOPT = JuMP.with_optimizer(Ipopt.Optimizer, print_level = 0)
-const JUNIPER = JuMP.with_optimizer(Juniper.Optimizer, nl_solver=JuMP.with_optimizer(Ipopt.Optimizer, print_level=0), log_levels=[])
+const IPOPT = JuMP.optimizer_with_attributes(
+    Ipopt.Optimizer,
+    "print_level" => 0,
+)
 
+const JUNIPER = JuMP.optimizer_with_attributes() do
+    Juniper.Optimizer(nl_solver=IPOPT, log_levels=[])
+end
 
 const NLP_SOLVERS = [IPOPT]
 const MINLP_SOLVERS = [JUNIPER]
@@ -12,16 +20,13 @@ const MIPOLY_SOLVERS = [JUNIPER]
 
 @testset "JuMP Model Tests" begin
     @testset "$(solver): nlp" for solver in NLP_SOLVERS
-        #MINLPTests.test_nlp(solver)
         MINLPTests.test_nlp(solver, exclude = [
             "005_011",  # Uses the function `\`
         ])
         MINLPTests.test_nlp_cvx(solver)
     end
     @testset "$(solver): nlp_mi" for solver in MINLP_SOLVERS
-        #MINLPTests.test_nlp_mi(solver)
         MINLPTests.test_nlp_mi(solver, exclude = [
-            "003_013",  # Bug in Juniper - handling of expression graph?
             "005_011",  # Uses the function `\`
             "006_010"   # Bug in Juniper - handling of user-defined functions.
         ])
